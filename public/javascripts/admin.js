b9a5ee14faa89a1a049251f9a88db6ba7548984b
Philip Sagstetter <pwsagstetter@peoplepc.com>
2014-11-18 23:51:10 +0000

    Initial commit

---
+++ public/javascripts/admin.js
@@ -0,0 +1,11 @@
+console.info('Welcome to the admin page.');
+
+var socket = io();
+var send = function(text){
+  socket.emit('msg', text);
+};
+socket.on('msg2', function(text){
+  console.warn(text);
+});
+
+
